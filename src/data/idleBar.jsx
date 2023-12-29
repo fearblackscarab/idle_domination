05d8a4a5961c285982127bd232237746266aa03c
fearblackscarab <fearblackscarab@gmail.com>
2023-12-29 09:30:21 -0600

    idle bar components started

--- src/data/idleBar.jsx
+++ src/data/idleBar.jsx
@@ -1,13 +1,57 @@
-const IdleBar = (props) => {
-    // idleBarMode
-    // idleBarSpeed
-    // idleAmount
-    // idleAmountCost
-    const [idleBarMode, setIdleBarMode] = useState('idle');
-    const [idleAmount, setIdleAmount] = useState(1);
-    const [idleAmountCost, setIdleAmountCost] = useState(10);
-    const [idleBarSpeed, setIdleBarSpeed] = useState(5000);
-    // idle iinterval auto increase currency
+import { useState } from "react";
 
+const IdleBar = (props) => {
+    // idle interval auto increase currency
+    const [currency, setCurrency] = useState(props.currency);
+
+
+    // will need to find a balance with the cost for the amount earned increasing the amount available per upgradeCost later on
+    // const [idleAmount, setIdleAmount] = useState(1);
+    // Will need to find a balanced cost calculation that isn't to low early on and not to high later on
+    const [idleAmountCost, setIdleAmountCost] = useState(1);
+
+    // idle bar speed should use a numberic value between 1-10 and will have a check somewhere to decide the speed based on what the value currently is
+    // const [idleBarSpeed, setIdleBarSpeed] = useState(0);
+    
+    // Maxed output of worker, any further upgradeCosts increase in cost but add a new worker
+    // const [idleBarMaxed, setIdleBarMaxed] = useState(False);
+
+    // number of workers
+    // const [workerCount, setWorkerCount] = useState(1);
+
+
+    const upgradeCost = () => {
+        if (currency >= idleAmountCost) {
+            setCurrency((prev) =>prev-idleAmountCost);
+            if (idleAmountCost <= 10) {
+                setIdleAmountCost((prev) => prev+4);
+            } else {
+                setIdleAmountCost((prev) => prev*2);
+            }
+        };
+        props.idleAmount + 1;
+    };
+
+    return(
+        <div className='idle-group'>
+                <div className="idle-bar-div" id="idleBarDiv">
+                    <div className="idle-progress-title">{props.name}</div>
+                    <div className="idle-bar">
+                        <div className="idle-progress" id="idleProgress">
+                        </div>
+                        <p className="idle-bar-amount" id="idleBarPercent">{currency}</p>
+                    </div>
+                </div>
+                <div className="cost" id="idleAmountCostDisplay">
+                    <div className="coin-container">
+                        <div className="bronze-coin-border coin-border">
+                            <div className="bronze-coin coin-img"><span className="coin-position">B</span></div>
+                            <input type="button" onClick={upgradeCost} className="btn idle-upgradeCost-button" value={idleAmountCost} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
 };
+
 export default IdleBar
--- src/pages/game/IdleGame.jsx
+++ src/pages/game/IdleGame.jsx
@@ -1,30 +1,25 @@
 import GameFunction from '../../function/Game'
+import IdleBar from '../../data/idleBar';
+import { useState } from 'react';
 const IdleGame = (props) => {
+    const [idleAmount, setIdleAmount] = useState(1);
+    const [currency, setCurrency] = useState(100000);
     return (
         <div>
             <header className="character-speak" id="characterSpeak">Welcome Mortal</header>
 
-            <div className='first bar'>
-                <div className="idle-bar-div" id="idleBarDiv">
-                    <div className="idle-progress-title">Anti-Idle
-                    </div>
-                    <div className="idle-bar">
-                        <div className="idle-progress" id="idleProgress">
-                        </div>
-                        <p className="idle-bar-percent" id="idleBarPercent">0%</p>
-                    </div>
-                </div>
-                <input type="button" onClick={GameFunction.earnProgress} className="btn idle-upgrade-button" value="Upgrade" />
-                <div className="cost" id="earnProgressCostDisplay">cost:${props.earnProgressCost}</div>
-            </div>
-            <p className="upgrade-text">Increase progress speed</p>
-            <input type='button' onClick={GameFunction.speedProgress} className="btn idle-upgrade-button" value="Upgrade" />
-            <div className="cost" id="speedProgressCostDisplay">cost:${props.speedProgressCost}</div>
+            <IdleBar name='worker' currency={currency} idleAmount={idleAmount}/>
 
             <p className="upgrade-title">New Feature</p>
-            <p className="upgrade-text" id="nextUnlockDisplay">${props.nextUnlock}</p>
-            <input type='button' onClick={GameFunction.unlockFeature} className="btn idle-upgrade-button" value="Upgrade" />
-            <div className="cost" id="unlockFeatureCostDisplay">cost:${props.unlockFeatureCost}</div>
+            <p className="upgrade-text" id="nextUnlockDisplay">{props.nextUnlock}</p>
+            <div className="cost" id="earnProgressCostDisplay">
+                <div className="coin-container">
+                    <div className="bronze-coin-border coin-border">
+                        <div className="bronze-coin coin-img"><span className="coin-position">B</span></div>
+                        <input type="button" onClick={GameFunction.earnProgress} className="btn idle-upgrade-button" value="Upgrade" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
